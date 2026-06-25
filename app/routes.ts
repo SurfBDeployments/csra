4b45f76c4abb98332c60d9ddd8436c7f0edb569e
killerbsurf-coder <killerbsurf@gmail.com>
2026-06-25 11:59:32 -0400

    added graphics/ updated packages

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,13 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  index("routes/default.tsx"),
+  route("home", "routes/home.tsx"),
+  route("projectsummary", "routes/ProjectSummary.tsx"),
+  route("proposalresults", "routes/ProposalResults.tsx"),
+  route("projectresults", "routes/ProjectResults.tsx"),
+  route("projectdetails", "routes/ProjectDetails.tsx"),
+  route("proposal", "routes/ProposalHome.tsx"),
+  route("ppt", "routes/Ppt.tsx"),
+
+] satisfies RouteConfig;
